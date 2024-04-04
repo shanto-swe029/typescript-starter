ba7d9e9273a7390be9631900e82dd84429db75e4
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-04-04 22:21:09 +0600

    NestJs First commit

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  /**
+   * By default, if any error happens while creating the application this app will exit with the code 1.
+   * To make it throw an error instead disable the option.
+   */
+  NestFactory.create(AppModule, { abortOnError: false });
 }
 bootstrap();
